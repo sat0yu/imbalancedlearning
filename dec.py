275d0885581e2e9b908bc2341285f616188d5291
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2013-09-25 14:57:18 +0900

    implement DEC

---
+++ dec.py
@@ -0,0 +1,86 @@
+#coding: utf-8;
+import numpy as np
+import os
+import sys
+from sklearn import svm
+
+import pyximport
+pyximport.install(setup_args={'include_dirs':[np.get_include()]}, inplace=True)
+from kernel import *
+from mlutil import *
+
+class DifferentErrorCost():
+    def __init__(self, kernel=None, beta=1., magic=1.):
+        self.kernel = kernel if kernel is not None else GaussKernel(beta)
+        self.magic = magic
+
+    def fit(self, posData, negData, label=[1,-1]):
+        # equip weight of each class
+        numPos, numNeg = float(len(posData)), float(len(negData))
+        if numPos < numNeg:
+            cPos, cNeg = 1., numPos / numNeg
+        else:
+            cPos, cNeg = numNeg / numPos, 1.
+
+        # equip sample with these label
+        posData = np.c_[ [label[0]]*posData.shape[0], posData]
+        negData = np.c_[ [label[1]]*negData.shape[0], negData]
+
+        # concatenate sample matrices
+        self.sample = np.r_[posData, negData]
+
+        # ready and fit SVM to given sample
+        self.clf = svm.SVC(kernel='precomputed', class_weight={label[0]:cPos, label[1]:cNeg})
+        gram = self.kernel.gram(self.sample[:,1:])
+        self.clf.fit(gram, self.sample[:,0])
+
+    def predict(self, target):
+        mat = self.kernel.matrix(target, self.sample[:,1:])
+        return self.clf.predict(mat)
+
+def procedure(numTest, numTrain, classRatio):
+    mean, cov = [-10, -10], [[50,0],[0,100]]
+    posDist = NormalDistribution(mean, cov)
+
+    mean, cov = [10, 10], [[100,0],[0,50]]
+    negDist = NormalDistribution(mean, cov)
+
+    id = ImbalancedData(posDist, negDist, classRatio)
+    trainset = id.getSample(numTrain)
+    testset = id.getSample(numTest)
+    label, X = trainset[:,0], trainset[:,1:]
+    answer, Y = testset[:,0], testset[:,1:]
+
+    print "given positive samples (train): ", len(label[label[:]==1])
+    print "given negative samples (train): ", len(label[label[:]==-1])
+    print "given positive samples (test): ", len(answer[answer[:]==1])
+    print "given negative samples (test): ", len(answer[answer[:]==-1])
+
+    # params definition
+    magic = 20000
+    beta = 0.005
+
+    # default SVM
+    clf = svm.SVC(kernel='rbf', gamma=beta)
+    clf.fit(X, label)
+    predict = clf.predict(Y)
+    acc,accPos,accNeg,g = evaluation(predict, answer)
+    print "[svm] ", "acc: ", acc
+    print "[svm] ", "acc on pos: ", accPos
+    print "[svm] ", "acc on neg: ", accNeg
+    print "[svm] ", "g-mean: ", g
+
+    # DEC
+    dec = DifferentErrorCost(beta=beta, magic=magic)
+    dec.fit(X[label[:]==1,:], X[label[:]==-1,:])
+    predict = dec.predict(Y)
+    acc,accPos,accNeg,g = evaluation(predict, answer)
+    print "[DEC] ", "acc: ", acc
+    print "[DEC] ", "acc on pos: ", accPos
+    print "[DEC] ", "acc on neg: ", accNeg
+    print "[DEC] ", "g-mean: ", g
+
+if __name__ == '__main__':
+    for cr in [1., 2., 5., 10., 20., 50., 100.]:
+        #procedure(5000, 1000, cr)
+        procedure(1250, 250, cr)
