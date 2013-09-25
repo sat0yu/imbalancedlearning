9aae411aa6dffc3fd64b562a74a1f60fc10da00e
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2013-09-25 15:17:56 +0900

    merge CIL procs. in cil.py

--- cil.py
+++ cil.py
@@ -2,14 +2,41 @@
 import numpy as np
 import os
 import sys
-from matplotlib import pyplot as plt
-from matplotlib import lines
 from sklearn import svm
 
 import pyximport
 pyximport.install(setup_args={'include_dirs':[np.get_include()]}, inplace=True)
 from kernel import *
 from mlutil import *
+
+class DifferentErrorCost():
+    def __init__(self, kernel=None, beta=1., magic=1.):
+        self.kernel = kernel if kernel is not None else GaussKernel(beta)
+        self.magic = magic
+
+    def fit(self, posData, negData, label=[1,-1]):
+        # equip weight of each class
+        numPos, numNeg = float(len(posData)), float(len(negData))
+        if numPos < numNeg:
+            cPos, cNeg = 1., numPos / numNeg
+        else:
+            cPos, cNeg = numNeg / numPos, 1.
+
+        # equip sample with these label
+        posData = np.c_[ [label[0]]*posData.shape[0], posData]
+        negData = np.c_[ [label[1]]*negData.shape[0], negData]
+
+        # concatenate sample matrices
+        self.sample = np.r_[posData, negData]
+
+        # ready and fit SVM to given sample
+        self.clf = svm.SVC(kernel='precomputed', class_weight={label[0]:cPos, label[1]:cNeg})
+        gram = self.kernel.gram(self.sample[:,1:])
+        self.clf.fit(gram, self.sample[:,0])
+
+    def predict(self, target):
+        mat = self.kernel.matrix(target, self.sample[:,1:])
+        return self.clf.predict(mat)
 
 class ProbFuzzySVM():
     def __init__(self, beta, magic):
@@ -76,6 +103,16 @@ def procedure(numTest, numTrain, classRatio):
     print "[svm] ", "acc on pos: ", accPos
     print "[svm] ", "acc on neg: ", accNeg
     print "[svm] ", "g-mean: ", g
+
+    # DEC
+    dec = DifferentErrorCost(beta=beta, magic=magic)
+    dec.fit(X[label[:]==1,:], X[label[:]==-1,:])
+    predict = dec.predict(Y)
+    acc,accPos,accNeg,g = evaluation(predict, answer)
+    print "[DEC] ", "acc: ", acc
+    print "[DEC] ", "acc on pos: ", accPos
+    print "[DEC] ", "acc on neg: ", accNeg
+    print "[DEC] ", "g-mean: ", g
 
     # ProvFuzzySVM
     pfsvm = ProbFuzzySVM(beta, magic)
