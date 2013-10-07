e59be731db743fbc2d5e7b1cb702a4523d36bbd9
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2013-10-07 18:47:04 +0900

    refactoring cil.py

--- cil.py
+++ cil.py
@@ -11,164 +11,146 @@ pyximport.install(setup_args={'include_dirs':[np.get_include()]}, inplace=True)
 from kernel import *
 from mlutil import *
 
-class DifferentErrorCost():
-    def __init__(self, kernel, C=1.):
+class KernelProbabilityFuzzySVM(FloatKernel):
+    def __init__(self, kernel):
         self.kernel = kernel
-        self.C = C
 
-    def fit(self, sample, label, class_label=[1,-1]):
-        # store given sample
-        self.sample = sample
-
-        # equip weight of each class
+    def class_weight(self, sample, label, class_label=[1,-1]):
         numPos = float(len(label[label[:]==class_label[0]]))
         numNeg = float(len(label[label[:]==class_label[1]]))
+
         if numPos < numNeg:
             cPos, cNeg = 1., numPos / numNeg
         else:
             cPos, cNeg = numNeg / numPos, 1.
 
-        # ready and fit SVM to given sample
-        self.clf = svm.SVC(kernel='precomputed', C=self.C, class_weight={class_label[0]:cPos, class_label[1]:cNeg})
-        gram = self.kernel.gram(self.sample)
-        self.clf.fit(gram, label)
+        return (cPos, cNeg)
 
-    def predict(self, target):
-        mat = self.kernel.matrix(target, self.sample)
-        return self.clf.predict(mat)
-
-class KernelProbabilityFuzzySVM():
-    def __init__(self, kernel, C=1.):
-        self.kernel = kernel
-        self.C = C
-
-    def fit(self, sample, label, class_label=[1,-1]):
-        # equip weight of each class
-        numPos = float(len(label[label[:]==class_label[0]]))
-        numNeg = float(len(label[label[:]==class_label[1]]))
-        if numPos < numNeg:
-            cPos, cNeg = 1., numPos / numNeg
-        else:
-            cPos, cNeg = numNeg / numPos, 1.
-
-        # sort given sample with their label
+    def sort_by_label(self, label, sample):
         dataset = np.c_[label, sample]
         dataset = dataset[dataset[:,0].argsort()]
-        label, self.sample = dataset[:,0], dataset[:,1:]
+        return (dataset[:,0], dataset[:,1:])
+
+    def fit(self, sample, label, C=1., class_label=[1,-1]):
+        # equip weight of each class
+        cPos, cNeg = self.class_weight(sample, label, class_label)
+
+        # sort given sample by their label
+        label, self.sample = self.sort_by_label(label, sample)
+
+        # count sample belong to each class
+        numPos = len(label[label[:]==class_label[0]])
+        numNeg = len(label[label[:]==class_label[1]])
 
         # calc. gram matrix and then sample_weight
         gram = self.kernel.gram(self.sample)
         nFront, nBack = (numNeg, numPos) if class_label[0] > class_label[1] else (numPos, numNeg)
-        boundary = int(nFront)
-        wFront = np.sum(gram[:boundary,:boundary], axis=0)
-        wBack = np.sum(gram[boundary:,boundary:], axis=0)
+        wFront = np.sum(gram[:nFront,:nFront], axis=0)
+        wBack = np.sum(gram[nFront:,nFront:], axis=0)
         weight = np.r_[wFront / nFront, wBack / nBack]
 
-        # ready and fit SVM to given sample
-        self.clf = svm.SVC(kernel='precomputed', C=self.C, class_weight={label[0]:cPos, label[1]:cNeg})
+        # ready and fit SVM
+        self.clf = svm.SVC(kernel='precomputed', C=C, class_weight={label[0]:cPos, label[1]:cNeg})
         self.clf.fit(gram, label, sample_weight=weight)
 
     def predict(self, target):
         mat = self.kernel.matrix(target, self.sample)
         return self.clf.predict(mat)
 
-def multiprocess(args):
-    classifer_generator,X,label,Y,answer,C,beta = args
+def multiproc(args):
+    beta, C, Y, answer, X, label = args
 
-    clf = classifer_generator(C=C, beta=beta)
-    clf.fit(X, label)
+    clf = KernelProbabilityFuzzySVM(GaussKernel(beta))
+    clf.fit(X, label, C=C)
     predict = clf.predict(Y)
 
-    ev = evaluation(predict, answer)
-    #print "C:%f,beta:%f,g:%f" % (C,beta,ev[-1])
-    #sys.stdout.flush()
-    return ev
-
-def parameter_search(X, label, classifer_generator, C=[], beta=[], nCV=5):
-    dataset = np.c_[label, X]
-    opt_C, opt_beta, maxScore = 0., 0., 0.
-    pool = multiprocessing.Pool(multiprocessing.cpu_count())
-    for _C in C:
-        for _beta in beta:
-            args = [ (classifer_generator,_X,_label,_Y,_answer,_C,_beta) for _Y,_answer,_X,_label in dataset_iterator(dataset, nCV) ]
-            scores = pool.map(multiprocess, args)
-
-            tmp = np.array(scores)
-            accP, accN = sum(tmp[:,1])/nCV, sum(tmp[:,2])/nCV
-            g = np.sqrt(accP * accN)
-            print "C:%f, beta:%f, g:%f" % (_C, _beta, g)
-
-            if g > maxScore:
-                maxScore, opt_C, opt_beta = g, _C, _beta
-
-    return (opt_C, opt_beta, maxScore)
-
-def gauss_svm(C, beta):
-    return svm.SVC(kernel="rbf", gamma=beta, C=C)
-
-def gauss_svm_dec(C, beta):
-    return DifferentErrorCost(GaussKernel(beta), C=C)
-
-def gauss_svm_kpfsvm(C, beta):
-    return KernelProbabilityFuzzySVM(GaussKernel(beta), C=C)
-
-def routine(name, classifer_generator, X, label, Y, answer, nCV=5):
-    # ready parameter search space
-    rough_C, rough_beta = [10**i for i in range(10)], [10**i for i in range(-9,1)]
-    narrow_space = np.linspace(-0.75, 0.75, num=7)
-
-    # rough search
-    opt_C, opt_beta, g = parameter_search(X, label, classifer_generator, C=rough_C, beta=rough_beta, nCV=nCV)
-    print "%s: C=%f, beta=%f, g=%f" % (name, opt_C, opt_beta, g)
-
-    # narrow search
-    _C = [opt_C*(10**i) for i in narrow_space]
-    _beta = [opt_beta*(10**i) for i in narrow_space]
-    opt_C, opt_beta, g = parameter_search(X, label, classifer_generator, C=_C, beta=_beta, nCV=nCV)
-    print "%s: C=%f, beta=%f, g=%f" % (name, opt_C, opt_beta, g)
-
-    # evaluation
-    clf = classifer_generator(opt_C, opt_beta)
-    clf.fit(X, label)
-    predict = clf.predict(Y)
+    #acc,accP,accN,g = evaluation(predict, answer)
+    #print "[C:%f]%f\t%f\t%f\t%f" % (C,acc,accP,accN,g)
+    #return (acc,accP,accN,g)
 
     return evaluation(predict, answer)
 
 def procedure(dataset, nCV=5, **kwargs):
-    scores = { "SVM":[], "DEC":[], "KPFSVM":[] }
+    # ready parameter search space
+    rough_C = [10**i for i in range(10)]
+    rough_beta = [10**i for i in range(-9,1)]
+    narrow_space = np.linspace(-0.75, 0.75, num=7)
+
+    # cross varidation
+    scores = []
     for Y,answer,X,label in dataset_iterator(dataset, nCV, **kwargs):
-        print "given positive samples (train): ", len(label[label[:]==1])
-        print "given negative samples (train): ", len(label[label[:]==-1])
-        print "given positive samples (test): ", len(answer[answer[:]==1])
-        print "given negative samples (test): ", len(answer[answer[:]==-1])
+        print "train samples (pos:%d, neg:%d)" % (len(label[label[:]==1]),len(label[label[:]==-1]))
+        print "test samples (pos:%d, neg:%d)" % (len(answer[answer[:]==1]),len(answer[answer[:]==-1]))
 
-        # default SVM
-        scores['SVM'].append( routine('SVM', gauss_svm, X, label, Y, answer, nCV=nCV) )
+        # ready parametersearch
+        pseudo = np.c_[label, X]
+        pool = multiprocessing.Pool()
+        opt_beta, opt_C, maxScore = 0., 0., -999.
 
-        # DEC
-        scores['DEC'].append( routine('DEC', gauss_svm_dec, X, label, Y, answer, nCV=nCV) )
+        # rough parameter search
+        for C in rough_C:
+            for beta in rough_beta:
+                args = [ (beta, C) + elem for elem in dataset_iterator(pseudo, nCV) ]
+                buf = pool.map(multiproc, args)
 
-        # KernelProbabilityFuzzySVM
-        scores['KPFSVM'].append( routine('KPFSVM', gauss_svm_kpfsvm, X, label, Y, answer, nCV=nCV) )
+                acc, accP, accN = np.average(np.array(buf), axis=0)[:3]
+                g = np.sqrt(accP * accN)
+                if g > maxScore:
+                    maxScore, opt_C, opt_beta = g, C, beta
+        print "[rough search] opt_beta:%f,\topt_C:%f,\tg:%f" % (opt_beta,opt_C,g)
 
-    for k, v in scores.items():
-        tmp = np.array(v)
-        print "%s:\t acc:\t %s" % (k, sum(tmp[:,0]) / nCV)
-        accP, accN = sum(tmp[:,1])/nCV, sum(tmp[:,2])/nCV
-        g = np.sqrt(accP * accN)
-        print "%s:\t accP:\t %s" % (k, accP)
-        print "%s:\t accN:\t %s" % (k, accN)
-        print "%s:\t g:\t %s" % (k, g)
+        # narrow parameter search
+        maxScore = -999.
+        for C in [opt_C*(10**j) for j in narrow_space]:
+            for beta in [opt_beta*(10**i) for i in narrow_space]:
+                args = [ (beta, C) + elem for elem in dataset_iterator(pseudo, nCV) ]
+                buf = pool.map(multiproc, args)
+
+                acc, accP, accN = np.average(np.array(buf), axis=0)[:3]
+                g = np.sqrt(accP * accN)
+                if g > maxScore:
+                    maxScore, opt_C, opt_beta = g, C, beta
+        print "[narrow search] opt_beta:%f,\topt_C:%f,\tg:%f" % (opt_beta,opt_C,g)
+
+        # classify using searched params
+        gk = GaussKernel(opt_beta)
+        clf = KernelProbabilityFuzzySVM(gk)
+        clf.fit(X, label)
+        predict = clf.predict(Y)
+        scores.append( evaluation(predict, answer) )
+
+    # average evaluation score
+    acc, accP, accN = np.average(np.array(scores), axis=0)[:3]
+    g = np.sqrt(accP * accN)
+    print "acc:%f,\taccP:%f,\taccN:%f,\tg:%f" % (acc,accP,accN,g)
 
 if __name__ == '__main__':
     posDist = NormalDistribution([-10, -10], [[50,0],[0,100]])
     negDist = NormalDistribution([10, 10], [[100,0],[0,50]])
     id = ImbalancedData(posDist, negDist, 50.)
     dataset = id.getSample(5000)
-    procedure(dataset, nCV=5, label_index=0)
+    procedure(dataset, nCV=4, label_index=0)
 
     page = Dataset("data/page-blocks.rplcd", label_index=-1, dtype=np.float)
-    procedure(page.raw, 5, label_index=-1)
+    procedure(page.raw, label_index=-1)
 
     yeast = Dataset("data/yeast.rplcd", label_index=-1, usecols=range(1,10), dtype=np.float)
-    procedure(yeast.raw, 5, label_index=-1)
+    procedure(yeast.raw, label_index=-1)
+
+    abalone = Dataset("data/abalone.rplcd", label_index=-1, usecols=range(1,9), delimiter=',', dtype=np.float)
+    procedure(abalone.raw, label_index=-1)
+
+    ecoli = Dataset("data/ecoli.rplcd", label_index=-1, usecols=range(1,9), dtype=np.float)
+    procedure(ecoli.raw, label_index=-1)
+
+    transfusion = Dataset("data/transfusion.rplcd", label_index=-1, delimiter=',', skiprows=1, dtype=np.float)
+    procedure(transfusion.raw, label_index=-1)
+
+    haberman = Dataset("data/haberman.rplcd", label_index=-1, delimiter=',', dtype=np.float)
+    procedure(haberman.raw, label_index=-1)
+
+    waveform = Dataset("data/waveform.rplcd", label_index=-1, delimiter=',', dtype=np.float)
+    procedure(waveform.raw, label_index=-1)
+
+    pima = Dataset("data/pima-indians-diabetes.rplcd", label_index=-1, delimiter=',', dtype=np.float)
+    procedure(pima.raw, label_index=-1)
