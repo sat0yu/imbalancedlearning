9df033fa4564d6122be20ef49c062b6804a77112
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2013-10-16 17:40:07 +0900

    rearrange examination dataset

--- vec_exp.py
+++ vec_exp.py
@@ -104,28 +104,28 @@ if __name__ == '__main__':
     negDist = NormalDistribution([10, 10], [[100,0],[0,50]])
     id = ImbalancedData(posDist, negDist, 50.)
     dataset = id.getSample(5000)
-    procedure('gaussian mix.',dataset, nCV=4, label_index=0)
-
-    page = Dataset("data/page-blocks.rplcd", label_index=-1, dtype=np.float)
-    procedure('page-block',page.raw, label_index=-1)
-
-    yeast = Dataset("data/yeast.rplcd", label_index=-1, usecols=range(1,10), dtype=np.float)
-    procedure('yeast',yeast.raw, label_index=-1)
-
-    abalone = Dataset("data/abalone.rplcd", label_index=-1, usecols=range(1,9), delimiter=',', dtype=np.float)
-    procedure('abalone',abalone.raw, label_index=-1)
+    procedure('gaussian mix.', dataset, nCV=4, label_index=0)
 
     ecoli = Dataset("data/ecoli.rplcd", label_index=-1, usecols=range(1,9), dtype=np.float)
-    procedure('ecoli',ecoli.raw, label_index=-1)
+    procedure('ecoli', ecoli.raw, label_index=-1)
 
     transfusion = Dataset("data/transfusion.rplcd", label_index=-1, delimiter=',', skiprows=1, dtype=np.float)
-    procedure('transfusion',transfusion.raw, label_index=-1)
+    procedure('transfusion', transfusion.raw, label_index=-1)
 
     haberman = Dataset("data/haberman.rplcd", label_index=-1, delimiter=',', dtype=np.float)
-    procedure('haberman',haberman.raw, label_index=-1)
-
-    waveform = Dataset("data/waveform.rplcd", label_index=-1, delimiter=',', dtype=np.float)
-    procedure('waveform',waveform.raw, label_index=-1)
+    procedure('haberman', haberman.raw, label_index=-1)
 
     pima = Dataset("data/pima-indians-diabetes.rplcd", label_index=-1, delimiter=',', dtype=np.float)
-    procedure('pima',pima.raw, label_index=-1)
+    procedure('pima', pima.raw, label_index=-1)
+
+    yeast = Dataset("data/yeast.rplcd", label_index=-1, usecols=range(1,10), dtype=np.float)
+    procedure('yeast', yeast.raw, label_index=-1)
+
+    page = Dataset("data/page-blocks.rplcd", label_index=-1, dtype=np.float)
+    procedure('page-block', page.raw, label_index=-1)
+
+    abalone = Dataset("data/abalone.rplcd", label_index=-1, usecols=range(1,9), delimiter=',', dtype=np.float)
+    procedure('abalone', abalone.raw, label_index=-1)
+
+    waveform = Dataset("data/waveform.rplcd", label_index=-1, delimiter=',', dtype=np.float)
+    procedure('waveform', waveform.raw, label_index=-1)
