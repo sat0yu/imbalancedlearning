cfc14283275e40fd3024cfdb81d7ce1b1d4a6b3b
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2013-10-16 17:15:01 +0900

    bugfix in procedure(), until now, I have used optimized params to KPFSVM even though these params are optimazied for DEC...

--- vec_exp.py
+++ vec_exp.py
@@ -15,12 +15,15 @@ from cil import *
 def multiproc(args):
     rough_C, beta, Y, answer, X, label = args
 
-    clf = DifferentErrorCosts( GaussKernel(beta) )
-    gram = clf.precompute(X)
+    clf = KernelProbabilityFuzzySVM( GaussKernel(beta) )
+    #clf = DifferentErrorCosts( GaussKernel(beta) )
+    X, gram, label, weight = clf.precompute(X, label)
+    #gram = clf.precompute(X)
 
     res = []
     for _C in rough_C:
-        clf.fit(X, label, C=_C, gram=gram)
+        clf.fit(X, label, C=_C, gram=gram, sample_weight=weight)
+        #clf.fit(X, label, C=_C, gram=gram)
         predict = clf.predict(Y)
         res.append( (_C,)+evaluation(predict, answer) )
 
@@ -83,6 +86,7 @@ def procedure(dataname, dataset, nCV=5, **kwargs):
 
         # classify using searched params
         gk = GaussKernel(opt_beta)
+        #clf = DifferentErrorCosts(gk)
         clf = KernelProbabilityFuzzySVM(gk)
         clf.fit(X, label, C=opt_C)
         predict = clf.predict(Y)
